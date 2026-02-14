8ef1b85c58a3ee60c2d9ac0b733c99f86ecc5b3e
Nick Hackford <nick.hackford@gmail.com>
2026-02-13 21:13:10 -0500

    feat(bookmarklets): add environment switching bookmarklets and refactor URL handling

--- files/local/bookmarklets/wt1.js
+++ files/local/bookmarklets/wt1.js
@@ -1,5 +1,5 @@
 var url = new URL(window.location.href);
 var base = url.hostname.split('.').slice(-2).join('.');
-url.hostname = 'local.' + base;
+url.hostname = 'wt1.local.app.' + base;
 url.port = '';
 window.open(url.href, '_blank');
